a12c4fcb2302a10b778314c710980456bba9c863
Vsim <73603125+Vsimpro@users.noreply.github.com>
2021-03-12 17:04:46 +0200

    Making comments visible!

--- Code/reader.py
+++ Code/reader.py
@@ -1,7 +1,10 @@
 import time
 import requests as req
 
-# 
+# The site you want to scrape should be added here. 
+# If you run the test locally, you can add:
+# http://localhost:5000
+
 url = "http://your-url-here/"
 
 def listen():
@@ -10,13 +13,15 @@ def listen():
     if "!pC2" in content:  
         content = content.split("!pC2")
         return content[1]
+    # If the site does not contain "!pC2", this returns some value to keep the code running.
     return content[0]
     
-
 def main():
     while True:
         command = listen()
+        # You can change the interval of the ping by changing the value of this sleep()
         time.sleep(1)   
+        # Here you can set up your own actions and commands
         if command == "OFFLINE":
             break  
         if "ECHO" in command:
